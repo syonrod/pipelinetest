6c510a1a7d7d0f3101638e78029787144be38edd
syonrod <syoncode@gmail.com>
2019-07-04 15:15:10 -0600

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Test</title>
+</head>
+<body>
+    
+    <h2 class="date">
+        <?php echo date('l jS \of F Y h:i:s A'); ?>
+    </h2>
+</body>
+</html>
